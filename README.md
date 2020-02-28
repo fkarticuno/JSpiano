d28e54a768e5e29d07fd62a73f8c50064a265f6f
Frederick Articuno <mckiyoshi@gmail.com>
2020-02-27 21:28:41 -0500

    JSpiano finished for now

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # JSpiano
+
+This web-app uses HTML, JAVASCRIPT and CSS to simulate a piano and has an accompanying drum track to play along with. JSpiano tracks mouse clicks and key presses to play notes and adjust volume. 
+
+## [Try it yourself!](https://https://fkarticuno.github.io/JSpiano) 
+
+![image](https://fkarticuno.github.io/CodingPortfolio/Assets/Images/00.png)
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. They will also give an overview of some of the app's functionality. 
+
+### Prerequisites
+
+none
+
+### Installing
+
+Git clone the repository to your local machine: 
+
+HTTPS:
+```
+$ git clone https://github.com/fkarticuno/JSpiano.git
+```
+SSH:
+````
+$ git clone github.com:fkarticuno/JSpiano.git
+````
+
+## Built With
+[Tutorial](https://www.youtube.com/watch?v=vjco5yKZpU8)
+- [Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) - How to use flexbox to center elements
+- [Css Var](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties) - Using CSS variables to reduce redundant code
+- [OrangeFreeSounds](http://www.orangefreesounds.com/) - Playing audio in the browser
+- [Keybinds](https://css-tricks.com/snippets/javascript/javascript-keycodes/) - How to binding click and keypress events in JavaScript
+
+## Notes
+Please see https://github.com/fkarticuno/JSpiano/tree/master/imgs for progression images
+
+## Authors
+
+See the list of [contributors](https://github.com/fkarticuno/JSpiano/graphs/contributors) who participated in this project along with individual commit history. 
+
+## Planned updates
+
+At the moment, this app is the minimum viable product. We hope to make updates to improve the piano pitch range to simulate a full piano.
---
+++ script.js
@@ -0,0 +1,78 @@
+const WHITE_KEYS = ['z','x','c','v','b','n','m']
+const BLACK_KEYS = ['s','d','f','g','h']
+
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+const vols = document.querySelectorAll('.snd')
+const drums = document.getElementsByClassName('drums')[0]
+drums.loop = true;
+
+keys.forEach(key => {
+    key.addEventListener('click', ()=> playNote(key))
+    key.innerText = key.attributes[0].value   
+})
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0;
+    noteAudio.play();
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', ()=> {
+        key.classList.remove('active')
+    })
+}
+function changeVolume() {
+    var slider = document.getElementById('volSlider').value
+    drums.volume = slider / 100
+    vols.forEach(vol => {
+        vol.volume = slider / 100
+        var volControl = document.getElementById('volume')
+        volControl.innerText = 'Vol: '+ Math.round(slider)
+        console.log('Volume Change',vol.volume)
+    })
+}
+document.addEventListener('keydown', e => {
+    var slider = document.getElementById('volSlider').value
+    console.log(e.which, slider)
+    if ( (e.which === 37 || e.which === 40) && slider > 0) {
+        slider -- 
+        drums.volume = slider / 100 
+        vols.forEach(vol => {
+            vol.volume = slider / 100
+            var volControl = document.getElementById('volume')
+            document.getElementById('volSlider').value = Math.round(slider)
+            volControl.innerText = 'Vol: '+ Math.round(slider)
+            console.log('Volume Change',vol.volume)
+        })
+    } 
+    if ( (e.which === 39 || e.which === 38) && slider/100 <1.00  ) {
+        console.log('something')
+        slider ++
+        drums.volume = slider / 100
+        vols.forEach(vol => {
+            vol.volume = slider / 100
+            var volControl = document.getElementById('volume')
+            document.getElementById('volSlider').value = Math.round(slider)
+            volControl.innerText = 'Vol: '+ Math.round(slider)
+            console.log('Volume Change',vol.volume)
+        })
+    }
+})
+function toggleDrums() {
+    var curTime = document.getElementsByClassName('drums')[0].currentTime
+    if (curTime > 0) {
+        drums.pause()
+        drums.currentTime = 0
+    } else drums.play()
+}
+document.addEventListener('currentTime', k => {
+    console.log(k.currentTime)
+})
